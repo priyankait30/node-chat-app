63477c7165b0e43128f5b3a27f494a6944e55126
Gangwar <703168481@genpact.com>
2018-09-29 22:19:15 +0530

    Updates

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
@@ -7,8 +10,21 @@ console.log(__dirname + '/../public');
 console.log(publicPath);
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+
+
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
+server.listen(3000, () =>{
   console.log(`Server is up on port ${port}`);
-})
+});
